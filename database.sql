0391c4d89c7eaa6733ed2e4cf921f1fafa2c047d
Letícia Sayuri Kawakami <syrkleticia@gmail.com>
2019-09-07 22:02:48 -0300

    Criação da tabela e insert.

--- database.sql
+++ database.sql
@@ -13,3 +13,14 @@ create table users_insecure (
 
 insert into users_insecure (email, password)
 values ('allexxrodriguess@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'); -- password = 123
+
+
+drop table if exists user_secure;
+create table user_secure (
+	id int primary key auto_increment not null,
+	email varchar(255) not null unique,
+	password varchar(255) not null
+);
+
+insert into user_secure (email, password)
+values ('leticiateste@gmail.com', '6b82ad423213b3fb4b9cd1590b0a679c5f251467f05521088b8a6d666f9be55d'); -- password = leticiateste
